d21c275819f61268cb46d8b91d5cb6d5c0a6a7f2
Amanda <philhomic@qq.com>
2017-01-10 17:09:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+var wjData = require('./data_structure');
+console.log(wjData.data.pages[0].questions);
---
+++ 单选题结构.js
@@ -0,0 +1,37 @@
+{
+    "type": "radio",
+    "title": "&lt;p&gt;这里是一道单选题&lt;/p&gt;\n",
+    "description": "&lt;p&gt;这里是单选题的备注&lt;/p&gt;\n",
+    "required": true,
+    "goto": "",
+    "blank_setting": [],
+    "assess": {
+    "type": 0,
+        "score": 5,
+        "options": {
+        "o-100-ABCD": {
+            "flag": true
+        }
+    }
+},
+    "random": false,
+    "maxRow": "1",
+    "refer": null,
+    "options": [
+    {
+        "id": "o-100-ABCD",
+        "goto": "",
+        "display": [],
+        "noRandom": false,
+        "text": "&lt;p&gt;选项1&lt;/p&gt;\n"
+    },
+    {
+        "id": "o-101-EFGH",
+        "goto": "",
+        "display": [],
+        "noRandom": false,
+        "text": "&lt;p&gt;选项2&lt;/p&gt;\n"
+    }
+],
+    "id": "q-1-jQxt"
+}
